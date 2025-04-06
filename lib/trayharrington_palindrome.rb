4a024fb42ca68b8994644e67bdc978a0e0eec82c
Tray Harrington <traylon.harrington@gmail.com>
2025-04-06 13:59:24 -0500

    Initialize repository

---
+++ lib/trayharrington_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "trayharrington_palindrome/version"
+
+module TrayharringtonPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
